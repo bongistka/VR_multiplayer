e31b8614fc581e0e3b9bf53f5c3971d95b70c4b7
Barbora Darmová <barbora.darmova@virtuplex.cz>
2019-11-18 15:28:20 +0100

    Load and save simple

--- Assets/_Project/Scripts/AddInteractable.cs
+++ Assets/_Project/Scripts/AddInteractable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ public class AddInteractable : MonoBehaviour
 {
     public GameObject prefab;
     PlayerInteractable pi;
-    int objectID = 1;
+    int objectID = 0;
 
     void Start()
     {
@@ -16,10 +17,25 @@ public class AddInteractable : MonoBehaviour
     public void InstantiatePrefab()
     {
         GameObject objectClone = GameObject.Instantiate(prefab, new Vector3(0, 2, 0), Quaternion.identity);
+        GetNextObjectID();
         objectClone.name = prefab.gameObject.name + "_" + objectID;
         PlayerInteractable.AssetInScene go = new PlayerInteractable.AssetInScene(objectClone.gameObject.name, objectClone.transform.localScale, objectClone.transform.position, objectClone.transform.rotation);
         pi.assetsInScene.listOfAssets.Add(go);
-        objectID++;
         pi.UpdateAssetsPosition();
+    }
+
+    private void GetNextObjectID()
+    {
+        foreach (PlayerInteractable.AssetInScene asset in pi.assetsInScene.listOfAssets)
+        {
+            if (asset.name.Split('_')[0] == prefab.name)
+            {
+                if (asset.name.Split('_').Length>1)
+                {
+                    objectID = Mathf.Max(int.Parse(asset.name.Split('_')[1]), objectID);
+                }
+            }
+        }
+        objectID++;
     }
 }

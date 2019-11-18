c78a35d07e2dbcc9ca7f63fd39fd94d80117219c
Barbora Darmová <barbora.darmova@virtuplex.cz>
2019-11-18 12:20:25 +0100

    Fixed synchronisation interactable bug

---
+++ Assets/_Project/Scripts/OwnershipTransfer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Photon.Pun;
+
+public class OwnershipTransfer : MonoBehaviourPun
+{
+    private void OnHandHoverBegin(Hand hand)
+    {
+        base.photonView.RequestOwnership();
+    }
+}
